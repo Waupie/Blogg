09a34344d9985ba19d97204b4d9a4715ca598bc9
Maximilian Holm <maximilianholm@Maximilians-MacBook-Pro.local>
2019-08-17 12:19:13 +0200

    Added rgba to body when side navbar is active

--- js/index.js
+++ js/index.js
@@ -17,6 +17,7 @@ openSideNav.addEventListener('click', openNav);
 /* Set the width of the side navigation to 250px */
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
+    document.body.style.backgroundColor = 'rgba(0,0,0,0.4)';
 }
 
 let closeSideNav = document.querySelector('#closeSide');
@@ -25,5 +26,6 @@ closeSideNav.addEventListener('click', closeNav);
 /* Set the width of the side navigation to 0 */
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
+    document.body.style.backgroundColor = 'rgba(0,0,0,0)';
 }
 
